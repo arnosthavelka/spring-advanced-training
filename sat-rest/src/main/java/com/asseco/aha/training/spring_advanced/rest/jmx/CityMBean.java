2a6ba925098bf3ca94047830f2722a022cb72940
Arnošt Havelka <arnost.havelka@gmail.com>
2014-12-15 16:39:08 +0100

    + [sat-rest] example of jolokia usage

---
+++ sat-rest/src/main/java/com/asseco/aha/training/spring_advanced/rest/jmx/CityMBean.java
@@ -0,0 +1,41 @@
+package com.asseco.aha.training.spring_advanced.rest.jmx;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+import com.asseco.aha.training.spring_advanced.rest.domain.City;
+import com.asseco.aha.training.spring_advanced.rest.service.CityService;
+
+@Component
+@ManagedResource(description = "MBean about entity City")
+// objectName = "sat-rest:jmx=CityMBean",
+// jolokia: http://localhost:8080/jolokia/read/com.asseco.aha.training.sat-rest:name=cityMBean,type=CityMBean
+public class CityMBean {
+
+    @Autowired
+    private CityService cityService;
+
+    @Value("${sat-rest.cfg.author}")
+    private String author;
+
+    @ManagedAttribute
+    public String getAuthor() {
+        return author;
+    }
+
+    @ManagedAttribute
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @ManagedOperation(description = "Get City by ID")
+    public String getCityNameById(int id) {
+        City city = cityService.item(id);
+        return city.getName();
+    }
+
+}
