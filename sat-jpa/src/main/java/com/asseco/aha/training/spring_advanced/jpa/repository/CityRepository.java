9d2902e62a1ec6a2e4911725e353fae9476c6b81
Arnošt Havelka <arnost.havelka@gmail.com>
2015-01-14 16:43:40 +0100

    + [sat-jpa] example of JPA query definition in repository interface

--- sat-jpa/src/main/java/com/asseco/aha/training/spring_advanced/jpa/repository/CityRepository.java
+++ sat-jpa/src/main/java/com/asseco/aha/training/spring_advanced/jpa/repository/CityRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.asseco.aha.training.spring_advanced.jpa.domain.City;
 
@@ -17,8 +19,13 @@ public interface CityRepository extends JpaRepository<City, Long>, JpaSpecificat
 
     City findByNameAndCountryAllIgnoringCase(String name, String country);
 
+    // query is defined by @NamedQuery (in the entity City)
     City findByName(String name);
 
+    // query is defined by @NamedQuery (in the entity City)
     List<City> findByNameAndCountry(String name, String country);
+    
+    @Query("SELECT c FROM City c WHERE LOWER(c.name) = LOWER(:name)")
+    City retrieveByName(@Param("name") String name);
 
 }
--- sat-jpa/src/test/java/com/asseco/aha/training/spring_advanced/jpa/NamedQueryTests.java
+++ sat-jpa/src/test/java/com/asseco/aha/training/spring_advanced/jpa/NamedQueryTests.java
@@ -1,7 +1,7 @@
 package com.asseco.aha.training.spring_advanced.jpa;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 import java.util.List;
 
@@ -11,21 +11,33 @@ import com.asseco.aha.training.spring_advanced.jpa.domain.City;
 
 public class NamedQueryTests extends AbstractCityTests {
 
+	private void verifyCity(City city, String name, String country) {
+		assertThat(city.getName(), equalTo(name));
+        assertThat(city.getCountry(), equalTo(country));
+	}
+
     @Test
     public void testQueryCityByName() {
         City city = cityRepository.findByName("Miami");
-        assertThat(city.getName(), equalTo("Miami"));
-        assertThat(city.getCountry(), equalTo("USA"));
+        verifyCity(city, "Miami", "USA");
     }
 
     @Test
-    public void testQueryCityAndCountry() {
+    public void testEntityQueryCityAndCountry() {
         List<City> result = cityRepository.findByNameAndCountry("% %", "USA");
         assertThat(result.size(), equalTo(2));
 
         City newYork = result.get(0);
-        assertThat(newYork.getName(), equalTo("New York"));
-        assertThat(newYork.getCountry(), equalTo("USA"));
+        verifyCity(newYork, "New York", "USA");
+    }
+
+    @Test
+    public void testInterfaceQueryRetrieveByName() {
+    	City city = cityRepository.retrieveByName("prague");
+    	verifyCity(city, "Prague", "Czech Republic");
+    	
+    	city = cityRepository.findByName("prague");
+    	assertThat(city, is(nullValue()));
     }
 
 }
