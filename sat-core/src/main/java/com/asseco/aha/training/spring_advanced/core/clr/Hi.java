e3f6336a259261b1047e9025eef9207aceb50f4e
Arnošt Havelka <arnost.havelka@gmail.com>
2015-03-18 11:22:31 +0100

    + [sat-core] additional initialization logs added to CommandLineRunner example

--- sat-core/src/main/java/com/asseco/aha/training/spring_advanced/core/clr/Hi.java
+++ sat-core/src/main/java/com/asseco/aha/training/spring_advanced/core/clr/Hi.java
@@ -1,5 +1,7 @@
 package com.asseco.aha.training.spring_advanced.core.clr;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,11 @@ public class Hi implements CommandLineRunner {
 
 	/** Logger. */
 	private static final Logger LOG = LoggerFactory.getLogger(Hi.class);
+
+	@PostConstruct
+	void init() {
+		LOG.info("Initializing Hi bean ...");
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
