f6f733f37d74f95bb847663a752c61128827f363
Arnošt Havelka <arnost.havelka@gmail.com>
2015-03-18 10:58:40 +0100

    - [sat-spring-boot] refactoring of collided name with @SpringBootApplication

---
+++ sat-spring-boot/src/main/java/com/asseco/aha/training/spring_advanced/sb/SbApplication.java
@@ -0,0 +1,13 @@
+package com.asseco.aha.training.spring_advanced.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbApplication.class, args);
+    }
+
+}
