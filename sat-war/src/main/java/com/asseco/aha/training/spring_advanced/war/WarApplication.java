5f5e487ad311846ab900cc6a824737cca0f10718
Arnošt Havelka <arnost.havelka@gmail.com>
2015-06-24 15:21:56 +0200

    + [sat-war] example of deployment Spring Boot application to AS (as WAR)

---
+++ sat-war/src/main/java/com/asseco/aha/training/spring_advanced/war/WarApplication.java
@@ -0,0 +1,20 @@
+package com.asseco.aha.training.spring_advanced.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WarApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WarApplication.class);
+	}
+
+    public static void main(String[] args) {
+		SpringApplication.run(WarApplication.class, args);
+    }
+
+}
