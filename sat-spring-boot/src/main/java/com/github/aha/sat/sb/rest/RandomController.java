ea3452c95a11e046297c5de8df1bcf5c613b2a1e
Arnošt Havelka <arnost.havelka@gmail.com>
2016-12-21 16:04:20 +0100

    + [sat-spring-boot] update to parent 0.1.0 and source package renamed to com.github.aha.sat.sb

---
+++ sat-spring-boot/src/main/java/com/github/aha/sat/sb/rest/RandomController.java
@@ -0,0 +1,48 @@
+package com.github.aha.sat.sb.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomController {
+
+    // http://localhost:8080/random/value
+    @Value("${my.value}")
+    private String value;
+
+    // http://localhost:8080/random/number
+    @Value("${my.number}")
+    private Integer number;
+
+    // http://localhost:8080/random/long
+    @Value("${my.long}")
+    private Long longnumber;
+
+    // http://localhost:8080/random/int10
+    @Value("${my.number.less.than.ten}")
+    private Integer int10;
+
+    // http://localhost:8080/random/intRange
+    @Value("${my.number.in.range}")
+    private Integer intRange;
+
+    @RequestMapping("/random/{name}")
+    public String value(@PathVariable("name") String name) {
+        if ("value".equals(name)) {
+            return value;
+        } else if ("number".equals(name)) {
+            return number.toString();
+        } else if ("long".equals(name)) {
+            return longnumber.toString();
+        } else if ("int10".equals(name)) {
+            return int10.toString();
+        } else if ("intRange".equals(name)) {
+            return intRange.toString();
+        } else {
+            return "Unknown parameter!";
+        }
+    }
+
+}
