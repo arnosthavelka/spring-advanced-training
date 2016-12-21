dabdaefdcd6010dd3e4eb1a3bd253bb23ad70e89
Arnošt Havelka <arnost.havelka@gmail.com>
2016-12-21 16:08:39 +0100

    + [sat-spring-boot-admin] update to parent 0.1.0 and source package renamed to com.github.aha.sat.sba

---
+++ sat-spring-boot-admin/src/main/java/com/github/aha/sat/sba/rest/HelloController.java
@@ -0,0 +1,18 @@
+package com.github.aha.sat.sba.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${hello.name:Arny}")
+    private String name;
+
+    @RequestMapping("/hello")
+    String hello() {
+        return String.format("Hello %s!", name);
+    }
+
+}
