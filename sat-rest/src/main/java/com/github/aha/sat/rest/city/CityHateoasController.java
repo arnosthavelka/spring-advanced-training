de26aa84dce6b2e97af7f9bb6183dd8ee04b6dac
hh310 <arnost.havelka@deutsche-boerse.com>
2021-03-03 08:11:48 +0100

    #8 CityResourceAssembler removed

--- sat-rest/src/main/java/com/github/aha/sat/rest/city/CityHateoasController.java
+++ sat-rest/src/main/java/com/github/aha/sat/rest/city/CityHateoasController.java
@@ -1,5 +1,7 @@
 package com.github.aha.sat.rest.city;
 
+import static java.util.stream.Collectors.toList;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
@@ -9,6 +11,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.aha.sat.rest.city.resource.CityResource;
-import com.github.aha.sat.rest.city.resource.CityResourceAssembler;
 import com.github.aha.sat.rest.city.resource.CitySimpleResource;
 import com.github.aha.sat.rest.city.resource.CitySimpleResourceAssembler;
 import com.wordnik.swagger.annotations.ApiParam;
@@ -30,9 +32,6 @@ public class CityHateoasController {
     private CityService cityService;
 
 	@Autowired
-	private CityResourceAssembler assembler;
-
-	@Autowired
 	private CitySimpleResourceAssembler simpleAssembler;
 
 	/**
@@ -40,7 +39,7 @@ public class CityHateoasController {
 	 * http://localhost:8080/city/resources/?country=Spain
 	 * http://localhost:8080/city/resources/?sorting=id
 	 **/
-	@GetMapping(value = "/", produces = { "application/hal+json" })
+	@GetMapping(value = "/", produces = HAL_JSON_VALUE)
 	public CollectionModel<CitySimpleResource> search(
 			@ApiParam(name = "country", required = false) @PathParam("country") String country,
             @ApiParam(name = "sorting", required = false) @PathParam("sorting") String sorting) {
@@ -50,20 +49,20 @@ public class CityHateoasController {
 		return CollectionModel.of(resources, linkTo(CityHateoasController.class).withSelfRel());
     }
 
-	@GetMapping(value = "/all", produces = { "application/hal+json" })
+	@GetMapping(value = "/all", produces = HAL_JSON_VALUE)
 	public CollectionModel<CityResource> searchAll(
 			@ApiParam(name = "country", required = false) @PathParam("country") String country,
 			@ApiParam(name = "sorting", required = false) @PathParam("sorting") String sorting) {
 		List<City> data = cityService.search(country, sorting);
 
-		CollectionModel<CityResource> resources = assembler.toCollectionModel(data);
+		List<CityResource> resources = data.stream().map(c -> new CityResource(c)).collect(toList());
 		return CollectionModel.of(resources, linkTo(CityHateoasController.class).withSelfRel());
 	}
 
-	@GetMapping(value = "/{id}", produces = { "application/hal+json" })
-	public CityResource getOne(@PathVariable("id") long id) {
+	@GetMapping(value = "/{id}", produces = HAL_JSON_VALUE)
+	public EntityModel<CityResource> getOne(@PathVariable("id") long id) {
         City city = cityService.getOne(id);
-		return assembler.toModel(city);
+		return new CityResource(city).toResource();
     }
 
 	@DeleteMapping(value = "/{id}")
