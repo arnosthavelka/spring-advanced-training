20877b3982696d8ade68c116450ac9f8b67a8268
hh310 <arnost.havelka@deutsche-boerse.com>
2021-03-05 08:02:59 +0100

    #4 Sonar hints

--- sat-rest/src/main/java/com/github/aha/sat/rest/city/resource/CityEntityResource.java
+++ sat-rest/src/main/java/com/github/aha/sat/rest/city/resource/CityEntityResource.java
@@ -9,7 +9,10 @@ import org.springframework.hateoas.server.core.Relation;
 import com.github.aha.sat.rest.city.City;
 import com.github.aha.sat.rest.city.CityHateoasController;
 
+import lombok.EqualsAndHashCode;
+
 @Relation(collectionRelation = "cities")
+@EqualsAndHashCode(callSuper = false)
 public class CityEntityResource extends RepresentationModel<CityEntityResource> {
 
 	private City city;
