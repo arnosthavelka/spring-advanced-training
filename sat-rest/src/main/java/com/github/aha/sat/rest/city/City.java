4b0e729e539ae0ea6724cfdb871a4d59241d1293
hh310 <arnost.havelka@deutsche-boerse.com>
2021-03-12 08:39:17 +0100

    #6 additional Swagger improvements

--- sat-rest/src/main/java/com/github/aha/sat/rest/city/City.java
+++ sat-rest/src/main/java/com/github/aha/sat/rest/city/City.java
@@ -10,11 +10,7 @@ import javax.persistence.Id;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.github.aha.sat.rest.city.resource.CityProjections.Basic;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 @Entity
-@ApiModel
 public class City implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -22,20 +18,16 @@ public class City implements Serializable {
     @Id
     @GeneratedValue
 	@JsonView(Basic.class)
-    @ApiModelProperty(position = 1, required = true, value = "ID of the city")
     private Long id;
 
     @Column(nullable = false)
 	@JsonView(Basic.class)
-    @ApiModelProperty(position = 2, required = true, value = "Name of the city")
     private String name;
 
     @Column(nullable = true)
-    @ApiModelProperty(position = 4, required = false, value = "State where the city belongs")
     private String state;
 
     @Column(nullable = false)
-    @ApiModelProperty(position = 3, required = true, value = "Country where the city belongs")
     private String country;
 
     protected City() {
--- sat-rest/src/main/java/com/github/aha/sat/rest/config/SpringFoxConfig.java
+++ sat-rest/src/main/java/com/github/aha/sat/rest/config/SpringFoxConfig.java
@@ -6,7 +6,9 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -22,15 +24,8 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @see https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
  */
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
-
-//    @Autowired
-//    private SpringSwaggerConfig springSwaggerConfig;
-//
-//    @Bean
-//    public SwaggerSpringMvcPlugin customImplementation() {
-//        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(".*city.*");
-//    }
 
 	@Bean
 	public Docket api() {
