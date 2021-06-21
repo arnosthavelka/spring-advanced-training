8070a819c688672e577ba53c998c633674142e1f
hh310 <arnost.havelka@deutsche-boerse.com>
2021-06-21 09:03:15 +0200

    + [elk] searchByCountry test added

--- sat-elk/src/test/java/com/github/aha/sat/elk/city/CityServiceTest.java
+++ sat-elk/src/test/java/com/github/aha/sat/elk/city/CityServiceTest.java
@@ -3,16 +3,22 @@ package com.github.aha.sat.elk.city;
 import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.data.domain.Pageable.unpaged;
 
 import java.util.UUID;
 
+import org.elasticsearch.common.collect.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,6 +28,7 @@ import com.github.aha.sat.elk.ElkException;
 class CityServiceTest {
 
 	private static final String CITY_ID = UUID.randomUUID().toString();
+	private static final String CITY_COUNTRY = "Germany";
 
 	@Mock
 	CityRepository repository;
@@ -53,6 +60,17 @@ class CityServiceTest {
 			verify(repository).findById(CITY_ID);
 		}
 
+	}
+
+	@Test
+	void searchByCountry() {
+		City city = new City();
+		given(repository.findByCountry(eq(CITY_COUNTRY), any())).willReturn(new PageImpl<City>(List.of(city)));
+
+		Page<City> result = service.searchByCountry(CITY_COUNTRY, unpaged());
+
+		assertThat(result.getContent().get(0)).isSameAs(city);
+		verify(repository).findByCountry(eq(CITY_COUNTRY), any());
 	}
 
 }
