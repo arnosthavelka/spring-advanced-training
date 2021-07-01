35609dcd4886b11b1c395492b6b41cbebbcfc28a
hh310 <arnost.havelka@deutsche-boerse.com>
2021-07-01 08:54:17 +0200

    - test fixed & additional code cleaning

--- sat-core/src/test/java/com/github/aha/sat/core/controller/GreetingControllerTest.java
+++ sat-core/src/test/java/com/github/aha/sat/core/controller/GreetingControllerTest.java
@@ -5,46 +5,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.nio.charset.Charset;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@WebAppConfiguration
+@WebMvcTest(GreetingController.class)
 class GreetingControllerTest {
 
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
 	private MockMvc mockMvc;
-
-	@BeforeEach
-	void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
-
 
 	@Test
 	void testArny() throws Exception {
-		mockMvc.perform(get("/arny").contentType(contentType)).andExpect(status().isOk())
+		mockMvc.perform(get("/arny"))
+				.andExpect(status().isOk())
 				.andExpect(content().contentType(APPLICATION_JSON_VALUE))
 				.andExpect(content().string("Hello!"));
 	}
 
 	@Test
 	void testJuan() throws Exception {
-		mockMvc.perform(get("/juan").contentType(contentType)).andExpect(status().isOk())
+		mockMvc.perform(get("/juan"))
+				.andExpect(status().isOk())
 				.andExpect(content().contentType(APPLICATION_JSON_VALUE))
 				.andExpect(content().string("Cao!"));
 	}
