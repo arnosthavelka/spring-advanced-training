ce5aea3a95a106c0460172ff07e651e939802b00
hh310 <arnost.havelka@deutsche-boerse.com>
2022-03-18 08:40:17 +0100

    #29 code cleaning

--- sat-jpa/src/test/java/com/github/aha/sat/jpa/city/CityRepositoryExampleTests.java
+++ sat-jpa/src/test/java/com/github/aha/sat/jpa/city/CityRepositoryExampleTests.java
@@ -1,8 +1,5 @@
 package com.github.aha.sat.jpa.city;
 
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.StreamSupport.stream;
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -24,14 +21,9 @@ class CityRepositoryExampleTests extends AbstractCityRepositoryTests {
 		var exampleCity = new City();
 		exampleCity.setState("Queensland");
 
-		Iterable<City> iterableResult = cityRepository.findAll(Example.of(exampleCity));
-		List<City> result = convertToList(iterableResult);
+		List<City> result = cityRepository.findAll(Example.of(exampleCity));
 
 		verifyFirstCityInCollection(result, "Brisbane", "Australia");
-	}
-
-	private <T> List<T> convertToList(Iterable<T> result) {
-		return stream(result.spliterator(), false).collect(toList());
 	}
 
 //	private Example<City> buildFilterByExample(City city) {
