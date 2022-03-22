f17e7b4a0feada1c00cc5ad7376e04256daa85ce
hh310 <arnost.havelka@deutsche-boerse.com>
2022-03-22 08:55:29 +0100

    + Bump up to SB 2.6.5

--- sat-jpa/src/main/java/com/github/aha/sat/jpa/city/CityCustomRepositoryImpl.java
+++ sat-jpa/src/main/java/com/github/aha/sat/jpa/city/CityCustomRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.github.aha.sat.jpa.city;
 import static com.github.aha.sat.jpa.city.City_.country;
 import static com.github.aha.sat.jpa.city.City_.name;
 import static com.github.aha.sat.jpa.city.City_.state;
+import static com.github.aha.sat.jpa.city.QCity.city;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,8 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQuery;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,6 +38,16 @@ public class CityCustomRepositoryImpl implements CityCustomRepository {
 
 		query.where(predicates.toArray(new Predicate[0]));
 		return em.createQuery(query).getResultList();
+	}
+
+	public List<City> findUsaCitiesBy(String nameArgument, String stateArgument) {
+		return new JPAQuery<>(em)
+				.select(city)
+				.from(city)
+				.where(city.name.eq(nameArgument)
+						.and(city.state.eq(stateArgument))
+						.and(city.country.eq("USA")))
+				.fetch();
 	}
 
 }
