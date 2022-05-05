c54e448c80139600b1623e86731987593d04ae79
hh310 <arnost.havelka@deutsche-boerse.com>
2022-05-05 08:32:02 +0200

    PR #33 readme removed

--- sat-jpa/src/main/java/com/github/aha/sat/jpa/country/CountryCustomRepository.java
+++ sat-jpa/src/main/java/com/github/aha/sat/jpa/country/CountryCustomRepository.java
@@ -6,6 +6,6 @@ import lombok.NonNull;
 
 public interface CountryCustomRepository {
 
-	List<Country> findAllByBy(@NonNull String cityName, @NonNull String cityState);
+	List<Country> findAllCountriesBy(@NonNull String cityName, @NonNull String cityState);
 
 }
--- sat-jpa/src/test/java/com/github/aha/sat/jpa/country/CountryRepositoryTests.java
+++ sat-jpa/src/test/java/com/github/aha/sat/jpa/country/CountryRepositoryTests.java
@@ -2,6 +2,7 @@ package com.github.aha.sat.jpa.country;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,18 +16,58 @@ class CountryRepositoryTests {
 	protected CountryRepository cityRepository;
 
 	@Test
-	void findAllByBy() {
-		var cityName = "San Francisco";
+	void findByName() {
+		var countryName = "Japan";
 
-		var result = cityRepository.findAllByBy(cityName, "California");
+		var result = cityRepository.findByName(countryName);
 
-		assertThat(result)
-				.hasSize(1)
-				.first()
-				.satisfies(c -> {
-					assertThat(c.getId()).isEqualTo(3);
-					assertThat(c.getCities()).map(City::getName).contains(cityName);
-				});
+		assertThat(result.getId()).isEqualTo(4);
+		assertThat(result.getName()).isEqualTo(countryName);
+		assertThat(result.getCities()).hasSize(1);
+	}
+
+	@Test
+	void findAllByNameLike() {
+		var namePattern = "%an%";
+
+		var result = cityRepository.findAllByNameLike(namePattern);
+
+		assertThat(result).map(Country::getName).containsExactly("Canada", "Japan", "France", "Switzerland");
+	}
+
+	@Nested
+	class FindAllCountriesByTest {
+
+		private static final String USA = "USA";
+
+		@Test
+		void exactValues() {
+			var cityName = "San Francisco";
+
+			var result = cityRepository.findAllCountriesBy(cityName, "California");
+
+			assertThat(result)
+					.hasSize(1)
+					.first()
+					.satisfies(c -> {
+						assertThat(c.getId()).isEqualTo(3);
+						assertThat(c.getName()).isEqualTo(USA);
+						assertThat(c.getCities()).map(City::getName).contains(cityName);
+					});
+		}
+
+		@Test
+		void wildcard() {
+			var result = cityRepository.findAllCountriesBy("%an%", "%i%");
+
+			assertThat(result)
+					.hasSize(2)
+					.allSatisfy(c -> {
+						assertThat(c.getName()).isEqualTo(USA);
+						assertThat(c.getCities()).map(City::getName).contains("Atlanta", "San Francisco");
+					});
+		}
+
 	}
 
 }
