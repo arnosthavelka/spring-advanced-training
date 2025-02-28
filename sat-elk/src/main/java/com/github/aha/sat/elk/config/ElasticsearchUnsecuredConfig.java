5d75caebf0b42ccfac5e648834982048282bd8ad
Arnost Havelka <arnost.havelka@clearstream.com>
2025-02-28 08:35:50 +0100

    ElasticsearchUnsecuredConfig added

---
+++ sat-elk/src/main/java/com/github/aha/sat/elk/config/ElasticsearchUnsecuredConfig.java
@@ -0,0 +1,26 @@
+package com.github.aha.sat.elk.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@ConditionalOnProperty(name = "elk.security-enabled", havingValue = "false")
+@RequiredArgsConstructor
+public class ElasticsearchUnsecuredConfig extends ElasticsearchConfiguration {
+
+	@Getter
+	private final ElasticsearchProperties elkProperties;
+
+	@Override
+	public ClientConfiguration clientConfiguration() {
+		return ClientConfiguration.builder()
+				.connectedTo(elkProperties.getHost())
+				.build();
+	}
+	
+}
